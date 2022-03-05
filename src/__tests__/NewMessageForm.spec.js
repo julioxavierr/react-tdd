04293dc3e3713cf4ed97e81ea9bec12b91b1424a
Júlio Xavier <julioxavierr@gmail.com>
2022-03-05 13:25:50 -0300

    expect the form to call a callback

--- src/__tests__/NewMessageForm.spec.js
+++ src/__tests__/NewMessageForm.spec.js
@@ -4,13 +4,23 @@ import { NewMessageForm } from "../NewMessageForm";
 
 describe("<NewMessageForm />", () => {
   describe("clicking the send button", () => {
-    it("clears the text field", async () => {
-      render(<NewMessageForm />);
+    const onSend = jest.fn();
 
+    it("calls the send handler", async () => {
+      render(<NewMessageForm onSend={onSend} />);
+
+      // type message
       await userEvent.type(screen.getByTestId("messageText"), "New message");
+
+      // click button
       userEvent.click(screen.getByTestId("sendButton"));
 
+      // check that the message was cleared
       expect(screen.getByTestId("messageText").value).toEqual("");
+
+      // expect it to have called the send handler
+      expect(onSend).toHaveBeenCalledTimes(1);
+      expect(onSend).toHaveBeenCalledWith("New message");
     });
   });
 });
